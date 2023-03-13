45f8003d0f9deebebe0d69d09c1a3d9a2e8d1147
George Samuel <geosam2708@gmail.com>
2023-03-13 10:26:31 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(names,eventName){
+    let newArray = [];
+    for (let count = 0; count < names.length; count++) {
+        let message = `Thank you, ${names[count]}, for the wonderful surprise gift!`;
+        newArray.push(message);
+    }
+    return newArray;
+}
+
+function countDown(number){
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    }
+}
